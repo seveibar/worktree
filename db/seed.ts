6170e1640ee9cb7709a444a25fd8ab5c4fa597f5
seveibar <seveibar@gmail.com>
2021-08-22 00:41:39 -0700

    wip converting seed

--- db/seed.ts
+++ db/seed.ts
@@ -1,18 +1,58 @@
 import getDB, { Knex } from "pgknexlove"
-import { Account, AccountAPIKey } from "~/types"
+import { Account, AccountAPIKey, Tree } from "~/types"
 
 export default async (db: Knex) => {
-  const acc = await db<Account>("account")
-    .innerJoin<AccountAPIKey>(
-      "account_api_key",
-      "account_api_key.account_id",
-      "account.id"
-    )
-    .select(["email", "key_string"])
-    .where({ account_id: "asd" })
-    .first()
+  await db<Account>("account").insert({ account_name: "user_1" })
+  await db<Account>("account").insert({ account_name: "user_2" })
+  await db<Account>("account").insert({ account_name: "user_3" })
 
-  acc.email // good!
-  acc.key_string // good!
-  acc.name
+  for (const [account_name, key_string] of [
+    ["KEY1", "user_1"],
+    ["KEY2", "user_2"],
+    ["KEY3", "user_3"],
+  ]) {
+    await db<AccountAPIKey>("account_api_key")
+      .update({ key_string })
+      .where({
+        account_id: db("account")
+          .select("account_id")
+          .where({ account_name }) as any,
+      })
+  }
+
+  await db<Tree>("tree").insert({
+    tree_name: "Test Tree",
+    tree_key: "test_tree",
+    owner_id: db("account")
+      .select("account_id")
+      .where("account_name", "user_1") as any,
+    tree_def: {
+      "Test Tree Start": {
+        name: "Test Tree Start",
+        order: 0,
+        description: "Some description",
+        requirements: {
+          "category1.meter1": {
+            mustBeAtleast: 15,
+          },
+          "category1.meter2": {
+            mustIncreaseBy: 50,
+          },
+        },
+      },
+      "Test Tree End": {
+        name: "Test Tree End",
+        order: 0,
+        parent: "Test Tree Start",
+        requirements: {
+          "category1.meter1": {
+            mustBeAtleast: 50,
+          },
+          "category2.meter1": {
+            mustBeAtleast: 100,
+          },
+        },
+      },
+    },
+  })
 }
